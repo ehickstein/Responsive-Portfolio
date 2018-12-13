a6655b711190a5df5586c6352a640e0b4b7385f8
ehickstein <38333223+ehickstein@users.noreply.github.com>
2018-12-12 19:52:33 -0600

    Revamped site

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    $(".parallax").parallax();
+    $("#parallax").parallax();
+
+    $('#contact-form').on('submit', (function(e) {
+      var name    = document.getElementById('inputName')
+      var email   = document.getElementById('inputEmail')
+      var message = document.getElementById('inputMessage')
+  
+      if (!name.value || !email.value || !message.value) {
+        alert("Please check your entries");
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: 'https://formspree.io/eric@thehicksteins.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        alert("Message sent");
+      }
+    }));
 });
